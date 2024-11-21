ada7ef966e3768281332f9a124c0f650e6a3b93d
bareerausman <bareera.usman12@icloud.com>
2024-11-21 15:05:37 +0000

    Initial commit: Spring Boot country population visualizer

---
+++ population-visualizer/src/main/java/com/elanco/population_visualizer/PopulationVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.elanco.population_visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulationVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulationVisualizerApplication.class, args);
+	}
+
+}
---
+++ population-visualizer/src/test/java/com/elanco/population_visualizer/PopulationVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elanco.population_visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopulationVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
